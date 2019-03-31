1fa3c3170e7535c0b29ad209b4fd78c68dc47c37
yongsk0066 <yongsk0066@naver.com>
2019-03-31 18:12:27 +0900

    search count

--- search/views.py
+++ search/views.py
@@ -10,29 +10,29 @@ def search(request):
     category = request.GET.get('choices', '')
     print(category)
 
-    # if category == 'Title':
-    #     qs = Post.objects.all()
-    #     q = request.GET.get('q', '')
-    #     if q:
-    #         result = []
-    #         qs = qs.filter(title__icontains=q)
-    #         for qp in qs:
-    #             result.append(get_object_or_404(Post, pk=qp.id))
-    #         return render(request, 'search/search_result.html', {
-    #             'result': result,
-    #         })
-    #
-    # elif category == 'Content':
-    #     qs = Post.objects.all()
-    #     q = request.GET.get('q', '')
-    #     if q:
-    #         result = []
-    #         qs = qs.filter(content__icontains=q)
-    #         for qp in qs:
-    #             result.append(get_object_or_404(Post, pk=qp.id))
-    #         return render(request, 'search/search_result.html', {
-    #             'result': result,
-    #         })
+    if category == 'Title':
+        qs = Post.objects.all()
+        q = request.GET.get('q', '')
+        if q:
+            result = []
+            qs = qs.filter(title__icontains=q)
+            for qp in qs:
+                result.append(get_object_or_404(Post, pk=qp.id))
+            return render(request, 'search/search_result.html', {
+                'result': result,
+            })
+
+    elif category == 'Content':
+        qs = Post.objects.all()
+        q = request.GET.get('q', '')
+        if q:
+            result = []
+            qs = qs.filter(content__icontains=q)
+            for qp in qs:
+                result.append(get_object_or_404(Post, pk=qp.id))
+            return render(request, 'search/search_result.html', {
+                'result': result,
+            })
 
     if category == 'User':
         qs = User.objects.all()
